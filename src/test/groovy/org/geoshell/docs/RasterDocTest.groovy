b880ff2c7e3fe6c2129b4e19d48e1c38f79493d5
jericks <jared.erickson@gmail.com>
2019-12-30 16:37:56 -0800

    Add raster info and value examples to docs

--- src/test/groovy/org/geoshell/docs/RasterDocTest.groovy
+++ src/test/groovy/org/geoshell/docs/RasterDocTest.groovy
@@ -35,4 +35,27 @@ class RasterDocTest extends AbstractDocTest {
         ])
     }
 
+    @Test
+    void info() {
+        run("raster_info", [
+                "format open --name earth --input src/test/resources/earth.tif",
+                "raster open --format earth --raster earth --name earth",
+                "raster info --name earth",
+                "raster close --name earth",
+                "format close --name earth"
+        ])
+    }
+
+    @Test
+    void value() {
+        run("raster_value", [
+                "format open --name earth --input src/test/resources/earth.tif",
+                "raster open --format earth --raster earth --name earth",
+                "raster value --name earth --x 60 --y 45",
+                "raster value --name earth --x 10 --y 15 --type pixel",
+                "raster close --name earth",
+                "format close --name earth"
+        ])
+    }
+
 }
