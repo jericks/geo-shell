58c34b7412bad292d177296283fb96421d7755b1
jericks <jared.erickson@gmail.com>
2020-01-01 10:23:18 -0800

    Add doc examples for map add layer and add raster

--- src/test/groovy/org/geoshell/docs/MapDocTest.groovy
+++ src/test/groovy/org/geoshell/docs/MapDocTest.groovy
@@ -32,6 +32,40 @@ class MapDocTest extends AbstractDocTest {
         ])
     }
 
+    @Test
+    void addLayer() {
+        run("map_add_layer", [
+                "map open --name world",
+                "workspace open --name naturalearth --params examples/naturalearth.gpkg",
+                "layer open --workspace naturalearth --layer countries --name countries",
+                "layer style set --name countries --style examples/countries.sld",
+                "layer open --workspace naturalearth --layer ocean --name ocean",
+                "layer style set --name ocean --style examples/ocean.sld",
+                "map add layer --name world --layer ocean",
+                "map add layer --name world --layer countries",
+                "map draw --name world --file examples/map_add_layer.png",
+                "map close --name world"
+        ])
+        copyFile(new File("examples/map_add_layer.png"), new File("src/main/docs/images"))
+    }
 
+
+    @Test
+    void addRaster() {
+        run("map_add_raster", [
+                "map open --name world",
+                "format open --name earth --input src/test/resources/earth.tif",
+                "raster open --format earth --raster earth --name earth",
+                "map add raster --name world --raster earth",
+                "workspace open --name naturalearth --params examples/naturalearth.gpkg",
+                "layer open --workspace naturalearth --layer countries --name countries",
+                "style create --params \"stroke=black stroke-width=0.1\" --file examples/outline.sld",
+                "layer style set --name countries --style examples/outline.sld",
+                "map add layer --name world --layer countries",
+                "map draw --name world --file examples/map_add_raster.png",
+                "map close --name world"
+        ])
+        copyFile(new File("examples/map_add_raster.png"), new File("src/main/docs/images"))
+    }
 
 }
