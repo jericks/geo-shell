03ee8ffdde5de2889277de4c4a8c5a299d405284
jericks <jared.erickson@gmail.com>
2020-02-12 15:36:27 -0800

    Add raster envelope, style set and get examples to docs

--- src/test/groovy/org/geoshell/docs/RasterDocTest.groovy
+++ src/test/groovy/org/geoshell/docs/RasterDocTest.groovy
@@ -58,4 +58,53 @@ class RasterDocTest extends AbstractDocTest {
         ])
     }
 
+    @Test
+    void envelope() {
+        run("raster_envelope", [
+                "format open --name earth --input src/test/resources/earth.tif",
+                "raster open --format earth --raster earth --name earth",
+                "workspace open --name layers --params memory",
+                "raster envelope --name earth --output-workspace layers --output-name outline",
+                "style create --params \"stroke=black stroke-width=3\" --file examples/outline.sld",
+                "layer style set --name outline --style examples/outline.sld",
+                "map open --name map",
+                "map add raster --name map --raster earth",
+                "map add layer --name map --layer outline",
+                "map draw --name map --file examples/raster_envelope.png",
+                "map close --name map"
+        ])
+        copyFile(new File("examples/raster_envelope.png"), new File("src/main/docs/images"))
+    }
+
+    @Test
+    void setStyle() {
+        run("raster_style_set", [
+                "format open --name pierce_county --input src/test/resources/pc.tif",
+                "raster open --format pierce_county --raster pc --name pc",
+                "style raster colormap --raster pc --values \"25=#9fd182,470=#3e7f3c,920=#133912,1370=#08306b,1820=#fffff5\" --file examples/style_raster_colormap.sld",
+                "raster style set --name pc --style examples/style_raster_colormap.sld",
+                "map open --name map",
+                "map add raster --name map --raster pc",
+                "map draw --name map --file examples/raster_style_set.png",
+                "map close --name map"
+        ])
+        copyFile(new File("examples/raster_style_set.png"), new File("src/main/docs/images"))
+    }
+
+    @Test
+    void getStyle() {
+        run("raster_style_get", [
+                "format open --name pierce_county --input src/test/resources/pc.tif",
+                "raster open --format pierce_county --raster pc --name pc",
+                "style raster colormap --raster pc --values \"25=#9fd182,470=#3e7f3c,920=#133912,1370=#08306b,1820=#fffff5\" --file examples/style_raster_colormap.sld",
+                "raster style set --name pc --style examples/style_raster_colormap.sld",
+                "map open --name map",
+                "map add raster --name map --raster pc",
+                "map draw --name map --file examples/raster_style_set.png",
+                "map close --name map",
+                "raster style get --name pc --style examples/pc_style.sld"
+        ])
+        copyFile(new File("examples/raster_style_set.png"), new File("src/main/docs/images"))
+    }
+
 }
